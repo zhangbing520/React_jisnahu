51ec32242560ecaee17bb41f79eb1be3dcda8b05
zhangbing520 <2281287648@qq.com>
2022-03-27 16:55:27 +0800

    11

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@
  * @Autor: zhangbing
  * @Date: 2022-03-13 13:42:39
  * @LastEditors: zhangbing
- * @LastEditTime: 2022-03-14 10:48:32
+ * @LastEditTime: 2022-03-14 14:55:15
  */
 import React from 'react';
 import ReactDOM from 'react-dom';
 // import Todolist from './todolist';
-import App from './app'
+import App from './App.js'
 
 ReactDOM.render(
   <React.StrictMode>
