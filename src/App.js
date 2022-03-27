291f63ecfc384838a53b8761b8295981144af5e6
zhangbing520 <2281287648@qq.com>
2022-03-27 16:55:14 +0800

    11

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from 'react'
+import './style.css'
+import { CSSTransition, TransitionGroup } from 'react-transition-group'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      show: true,
+      list: []
+    }
+
+    this.handleToggle = this.handleToggle.bind(this)
+    this.handleAddItem = this.handleAddItem.bind(this)
+  }
+
+  render() {
+    return (
+      // <Fragment>
+      //   <div className={this.state.show ? 'show' : 'hide'}>hello</div>
+      //   <button onClick={this.handleToggle}>toggle</button>
+      // </Fragment>
+
+      <Fragment>
+        {/* <CSSTransition
+          in={this.state.show}
+          timeout={1000}
+          classNames='fade'
+          unmountOnExit
+          onEntered={(el) => el.style.color = 'blue'}
+          appear={true}
+        > */}
+          {/* <div>hello</div> */}
+
+          <TransitionGroup>
+            
+            {
+              this.state.list.map((item, index) => {
+                return (
+                  <CSSTransition
+                    timeout={1000}
+                    classNames='fade'
+                    unmountOnExit
+                    onEntered={(el) => el.style.color = 'blue'}
+                    appear={true}
+                    key={index}
+                  > 
+                    <div>{item}</div>
+                  </CSSTransition>
+                )
+              })
+            }
+          </TransitionGroup>
+          
+        {/* </CSSTransition> */}
+        <button onClick={this.handleAddItem}>toggle</button>
+      </Fragment>
+
+    )
+  }
+
+  handleToggle() {
+    this.setState({
+      show: this.state.show ? false : true
+    })
+  }
+
+  handleAddItem() {
+    this.setState((prevState) => {
+      return {
+        list: [...prevState.list, 'item']
+      }
+    })
+  }
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
+/*
+ * @Description: 
+ * @Autor: zhangbing
+ * @Date: 2022-03-13 13:42:39
+ * @LastEditors: zhangbing
+ * @LastEditTime: 2022-03-14 10:48:32
+ */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import Todolist from './todolist';
+import App from './app'
 
 ReactDOM.render(
   <React.StrictMode>
+    {/* <Todolist /> */}
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
